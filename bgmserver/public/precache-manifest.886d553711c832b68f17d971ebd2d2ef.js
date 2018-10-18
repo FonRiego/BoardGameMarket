c4f497dd4ae52881b72e559289b42a765dd9854c
Alfonso Mateo Riego <fonriego@gmail.com>
2018-10-18 18:07:17 +0200

    deploying

---
+++ bgmserver/public/precache-manifest.886d553711c832b68f17d971ebd2d2ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aad0638035c08f6955e5",
+    "url": "/static/js/main.aad06380.chunk.js"
+  },
+  {
+    "revision": "1c541b9ee6d4c8015044",
+    "url": "/static/js/1.1c541b9e.chunk.js"
+  },
+  {
+    "revision": "aad0638035c08f6955e5",
+    "url": "/static/css/main.f052b9e6.chunk.css"
+  },
+  {
+    "revision": "f0ab0bb8c06a475cb617afbf07271326",
+    "url": "/index.html"
+  }
+];
