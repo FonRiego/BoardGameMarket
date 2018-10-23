3965d4aab6f76ff936a5f8b10dfd9d0342d1f68c
Alfonso Mateo Riego <fonriego@gmail.com>
2018-10-23 23:38:16 +0200

    deploying

---
+++ bgmserver/public/precache-manifest.cdf3bf26e7f983ff25346a8f2207b9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a85ebb710f4e3b01823dc4778657a19",
+    "url": "/static/media/Carcassonne.0a85ebb7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4eab4da6a4431bddda3d",
+    "url": "/static/js/main.4eab4da6.chunk.js"
+  },
+  {
+    "revision": "d05a8db6765388db32a5",
+    "url": "/static/js/1.d05a8db6.chunk.js"
+  },
+  {
+    "revision": "4eab4da6a4431bddda3d",
+    "url": "/static/css/main.f89bb767.chunk.css"
+  },
+  {
+    "revision": "ca602ec1273badec03fed5fd75bb3ed2",
+    "url": "/index.html"
+  }
+];
