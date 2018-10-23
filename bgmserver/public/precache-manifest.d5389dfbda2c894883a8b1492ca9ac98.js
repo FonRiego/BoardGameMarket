eeee69d6718d924e7c95a18508cd34f40adebe60
Alfonso Mateo Riego <fonriego@gmail.com>
2018-10-23 14:28:46 +0200

    deploying to heroku

---
+++ bgmserver/public/precache-manifest.d5389dfbda2c894883a8b1492ca9ac98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a85ebb710f4e3b01823dc4778657a19",
+    "url": "/static/media/Carcassonne.0a85ebb7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ddd28ba79f95bd859440",
+    "url": "/static/js/main.ddd28ba7.chunk.js"
+  },
+  {
+    "revision": "c288a9ed4e3bcb42c61e",
+    "url": "/static/js/1.c288a9ed.chunk.js"
+  },
+  {
+    "revision": "ddd28ba79f95bd859440",
+    "url": "/static/css/main.6ef5b582.chunk.css"
+  },
+  {
+    "revision": "5cb4606cb910f306d167971bb85ce074",
+    "url": "/index.html"
+  }
+];
